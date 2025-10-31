0d2193ce716ac74198a80331a766a8e027cbbcb8
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2025-10-30 21:27:16 -0300

    feat: createProfile

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { randomUUID } from 'crypto';
+import { CreateProfileDto } from './dto/create-profile.dto';
 
 @Injectable()
 export class ProfilesService {
@@ -27,5 +28,12 @@ export class ProfilesService {
 
   findOne(id: string) {
     return this.profiles.find(profile => profile.id === id);
+  }
+
+  create(createProfileDto: CreateProfileDto) {
+    return this.profiles.push({
+      id: randomUUID(),
+      ...createProfileDto
+    });
   }
 }
